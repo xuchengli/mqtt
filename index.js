6260e51f5490b40a3f40273f919647bd9b56a0d1
Li Xu Cheng <lixucheng@ziggurat.cn>
2019-09-24 18:14:49 +0800

    add tls certs for mqtts connection

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const mqtt = require('mqtt');
-const client = mqtt.connect('ssl://emq.yfmen.com:8883');
+const fs = require('fs');
+const path = require('path');
 
-client.on('connect', function () {
-  client.subscribe('/d/r', function (err) {
-  });
-});
+const KEY = fs.readFileSync(path.join(__dirname, 'tls-artifacts/client.key'));
+const CERT = fs.readFileSync(path.join(__dirname, 'tls-artifacts/client.pem'));
+const TRUSTED_CA_LIST = fs.readFileSync(path.join(__dirname, 'tls-artifacts/ca.pem'));
 
+const options = {
+  port: 8883,
+  host: 'emq.yfmen.com',
+  key: KEY,
+  cert: CERT,
+  rejectUnauthorized: true,
+  // The CA list will be used to determine if server is authorized
+  ca: TRUSTED_CA_LIST,
+  protocol: 'mqtts'
+}
+const client = mqtt.connect(options);
+
+client.subscribe('/d/r');
 client.on('message', function (topic, message) {
   // message is Buffer
   console.log(message.toString());
   client.end();
+});
+
+client.on('connect', function () {
+  console.log('Connected');
 });
