4cc222ff4a86c0be33c7f468f7664d526a32fe1a
Li Xu Cheng <lixucheng@ziggurat.cn>
2019-09-25 12:11:07 +0800

    add protobuf message

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 const mqtt = require('mqtt');
 const fs = require('fs');
 const path = require('path');
+const protobuf = require('protobufjs');
 
 const KEY = fs.readFileSync(path.join(__dirname, 'tls-artifacts/client.key'));
 const CERT = fs.readFileSync(path.join(__dirname, 'tls-artifacts/client.pem'));
 const TRUSTED_CA_LIST = fs.readFileSync(path.join(__dirname, 'tls-artifacts/ca.pem'));
 
-const options = {
-  port: 8883,
-  host: 'emq.yfmen.com',
-  key: KEY,
-  cert: CERT,
-  rejectUnauthorized: false,
-  // The CA list will be used to determine if server is authorized
-  ca: TRUSTED_CA_LIST,
-  protocol: 'mqtts'
-}
-const client = mqtt.connect(options);
+protobuf.load("accessrecord.proto", function(err, root) {
+  if (err) throw err;
 
-client.subscribe('/d/r');
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString());
-  client.end();
-});
+  const AccessRecordMessage = root.lookupType("accessrecord.AccessRecord");
 
-client.on('connect', function () {
-  console.log('Connected');
+  const payload = {
+    id: 1,
+    sn: 2,
+    time: 20190925,
+    deviceId: 4,
+    opened: true,
+    codeType: 5
+  };
+
+  const errMsg = AccessRecordMessage.verify(payload);
+  if (errMsg) throw Error(errMsg);
+
+  const options = {
+    port: 8883,
+    host: 'emq.yfmen.com',
+    key: KEY,
+    cert: CERT,
+    rejectUnauthorized: false,
+    // The CA list will be used to determine if server is authorized
+    ca: TRUSTED_CA_LIST,
+    protocol: 'mqtts'
+  }
+  const client = mqtt.connect(options);
+
+  client.subscribe('/d/r');
+  client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString());
+    client.end();
+  });
+
+  client.on('connect', function () {
+    console.log('Connected');
+  });
 });
